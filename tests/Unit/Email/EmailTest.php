e9743e5ffebf8ef934575f5ab5835d0f6c5a357c
Rizart Dokollari <r.dokollari@gmail.com>
2021-03-06 03:03:34 +0000

    Enable unit test for checking type of email send access endpoint.

--- tests/Unit/Email/EmailTest.php
+++ tests/Unit/Email/EmailTest.php
@@ -1,13 +1,7 @@
 <?php
-/**
- * @author  Rizart Dokollari
- * @since   12/24/17
- */
 
-namespace Tests\Email;
+namespace Tests\Unit\Email;
 
-use ElasticEmail\Client;
-use ElasticEmail\ElasticEmailException;
 use ElasticEmail\Email\Email;
 use ElasticEmail\Email\Send;
 use Tests\TestCase;
@@ -17,9 +11,9 @@ class EmailTest extends TestCase
     /** @test */
     public function has_access_to_send_client()
     {
-        $this->expectException(ElasticEmailException::class);
+        $client = $this->mockElasticEmailAPIRequest();
 
-        $email = new Email(new Client('api-key'));
+        $email = new Email($client);
 
         $this->assertInstanceOf(Send::class, $email->send());
     }
