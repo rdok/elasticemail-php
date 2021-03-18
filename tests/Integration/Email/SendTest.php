2b9a60684b446e1278117b431bfd1a91e228de3c
Rizart Dokollari <r.dokollari@gmail.com>
2021-03-18 20:39:57 +0000

    Pass files to attach.

--- tests/Integration/Email/SendTest.php
+++ tests/Integration/Email/SendTest.php
@@ -38,14 +38,12 @@ class SendTest extends IntegrationTestCase
             __DIR__ . '/alpha-attachment.txt',
              __DIR__ . '/beta-attachment.txt'
         ];
-
         $elasticEmail = new ElasticEmail($_ENV['ELASTIC_EMAIL_API_KEY']);
         $response = $elasticEmail->email()->send([
             'to' => $_ENV['TEST_EMAIL_TO'],
             'from' => $_ENV['TEST_EMAIL_FROM'],
             'subject' => $subject,
             'bodyText' => 'Wake up Samurai!',
-            'mergeSourceFilename' => 'a',
         ], $attachments);
 
         $this->assertTrue($response->wasSuccessful());
